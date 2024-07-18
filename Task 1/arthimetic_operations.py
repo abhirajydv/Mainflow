55db14fcde9d628c830d407600f91c3aef14c681
abhirajydv <abhiraj6972@gmail.com>
2024-07-18 13:27:04 +0530

    first commit

---
+++ Task 1/arthimetic_operations.py
@@ -0,0 +1,10 @@
+#ARTHIMETIC OPERATIONS
+a = 10
+b = 5
+
+print(a+b) #Addition
+print(a-b) #Subtraction
+print(a*b) #Multiplication
+print(a/b) #Division
+print(a%b) #Modulous
+print(a**b) #Exponential
---
+++ Task 1/string_manipulation.py
@@ -0,0 +1,12 @@
+# String Manipulation
+a = "Hey! What happened?"
+b = "nothing!"
+
+print(a.capitalize())     # Capitalize
+print(a[5:])              # Slice
+print(a.upper())          # Upper Case
+print(a.lower())          # Lower Case
+print(a.strip())          # Remove Whitespace
+print(a.replace("W","H")) # Replace String
+print(a.split("!"))       # Split String
+print(a+" "+b)            # Concatination
